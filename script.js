93361b946937a7979d8b9a403bd17b7169bdf19f
Nayab Naeem <nayabnaeem666@gmail.com>
2026-07-01 01:24:52 +0500

    Added React.js as skill

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ window.addEventListener('load', () => {
 });
 
 // Skill bars animation
-const bars = ['b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12'];
+const bars = ['b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12','b13'];
 bars.forEach((id, i) => {
   setTimeout(() => {
     const bar = document.getElementById(id);
