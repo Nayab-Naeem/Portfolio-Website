44aa1d9b3b7b1687e0af3dc0d66d2edfcbcbe724
Nayab Naeem <nayabnaeem666@gmail.com>
2026-07-24 00:44:02 +0500

    Updated my skill section

--- script.js
+++ script.js
@@ -91,7 +91,12 @@ window.addEventListener('load', () => {
 });
 
 // Skill bars animation
-const bars = ['b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12','b13'];
+const bars = [
+  'b1','b2','b3','b4','b5','b6','b7',
+  'b8','b9','b10','b11','b12',
+  'b13','b14','b15','b16','b17'
+];
+
 bars.forEach((id, i) => {
   setTimeout(() => {
     const bar = document.getElementById(id);
